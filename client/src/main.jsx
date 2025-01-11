19d8bc279093a1e8659b057d47c9cde0f76af524
Ryan Charles <rynchrls18@gmail.com>
2025-01-11 09:29:45 +0800

    server port

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import theme from "../theme.js";
 import axios from "axios";
 
 axios.defaults.baseURL =
-  import.meta.env.VITE_SOCKET_HOST || "http://localhost:5000"; 
+  import.meta.env.VITE_SOCKET_HOST || "https://jervontis.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
